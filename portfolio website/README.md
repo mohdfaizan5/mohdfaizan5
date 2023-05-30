0cef1349c0c5e3a8c4404bd1f9d908d5b5b5e8fe
Mohammed Faizan <mohdfaizan.zprb2@gmail.com>
2023-05-30 09:58:11 +0530

    Portfolio V1.2

---
+++ portfolio website/README.md
@@ -0,0 +1,29 @@
+# portfolio-minimal [(Demo)]()
+This is for developers who think
+
+**"My work and code speak more than a Portfolio"**
+
+
+A small **minimal Portfolio** for developers who are tiring of thinking about creating their own new portfolio website.
+
+**NO WEB DEVELOPMENT SKILLS NEEDED** (If you do have, then can customize with your skills and ideas. :smile:)
+
+## Responsive
+It is fully responsive.
+
+### Laptops
+<img src="./README_DOCS/Lg.JPG"/>
+
+### Tabs
+<img src="./README_DOCS/md.JPG" width="500px">
+
+### Mobile
+<img src="./README_DOCS/sm.JPG" 
+  width="182px" >
+
+## Data for the Portfolio
+You don't need to surf through HTML docs and update the data. All the data needed for your portfolio must be updated in `data.js`. (_Right now just for functionality I provided my links. You can change it_)
+
+<img src="./README_DOCS/Data.JPG">
+
+---
---
+++ portfolio website/script.js
@@ -0,0 +1,63 @@
+const getDOM = selector => () => {
+  return document.querySelector(selector);
+};
+
+document.title = `${main.name} | ${main.role}`
+
+// Values DOM nodes
+const dom = {
+  main: {
+    name: getDOM('#main #name'),
+    mail: getDOM('#main #mail'),
+    img: getDOM('#main #img'),
+    role: getDOM('#main #role'),
+    connects: getDOM('#main #connects'),
+    links: getDOM('#main #links')
+  },
+  projects: getDOM('#projects'),
+  logo: getDOM('#projects-page #logo')
+};
+
+function assignDOM(dom, value, options) {
+  console.log('dom, value, img:', dom, value, img);
+
+  if (options && options.isImg) {
+    dom.src = value;
+    return;
+  }
+
+  if (options && options.isAdjacent) {
+    dom.insertAdjacentHTML('afterbegin', value);
+  }
+
+  dom.innerHTML = value;
+}
+
+// Assigning
+
+// MAIN
+
+assignDOM(dom.main.name(), main.name);
+assignDOM(dom.main.mail(), main.mail);
+dom.main.mail().href = `mailto:${main.mail}?Subject=Hello%20again`;
+assignDOM(dom.main.img(), main.img, { isImg: true });
+assignDOM(dom.main.role(), main.role);
+// assignDOM(dom.main.links(), main.links)
+
+// External Links (ICONS)
+const connectsDOM = main.connects
+  .map(
+    ({ name, iconName, link }) =>
+      `<a href=${link} target="_blank"><ion-icon name="${iconName}" title="${name}"></ion-icon></a>`
+  )
+  .join('\n');
+assignDOM(dom.main.connects(), connectsDOM);
+
+// Internal Links
+const getLinks = links =>
+  links
+    .map(({ name, link }) => `<a href="${link}" class="text-pink-500" >${name}</a>`)
+    .map((link, index, links) => index === links.length - 1 ? link: `${link} - `)
+    .join('\n');
+assignDOM(dom.main.links(), getLinks(main.links));
+
